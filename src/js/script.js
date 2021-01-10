9d8a922621baa4c58968b70bfe1607c5e9997f62
Rafal Hen <rafalhen@poczta.onet.pl>
2021-01-10 17:19:46 +0100

    bugs

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,3 @@
-const { render } = require("node-sass");
-
 {
   'use strict';
 
@@ -13,12 +11,44 @@ const { render } = require("node-sass");
     },
   };
 
-  render() {
-    const thisBooksList = this;
-    for(let book of thisBooksList.data){
-      const generatedHTML = templates.bookTemplate(book);
-      const element = utils.createDOMFromHTML(generatedHTML);
-      thisBooksList.menuContainer.appendChild(element);
-    };
+  const templates = {
+    bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
   };
+
+  class BooksList {
+    constructor(){
+      const thisBooksList = this;
+      thisBooksList.initData();
+      thisBooksList.getElements();
+      thisBooksList.render();
+    }
+
+    initData(){
+      const thisBooksList = this;
+      thisBooksList.data = dataSource.books;
+    }
+
+    getElements(){
+      const thisBooksList = this;
+
+      thisBooksList.menuContainer = document.querySelector(select.containerOf.bookList);
+    }
+
+    render(){
+      const thisBooksList = this;
+
+      for(let book of thisBooksList){
+        const generatedHTML = templates.bookTemplate(book);
+        const element = utils.createDOMFromHTML(generatedHTML);
+        thisBooksList.menuContainer.appendChild(element);
+      }
+    }
+  }
+
+  const app = {
+    initializeProject: function(){
+      new BooksList();
+    }
+  };
+  app.initializeProject();
 }
